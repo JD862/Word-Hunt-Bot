b7a461a4faa7fa66da7438f8567ea6ea03580fd0
JD862 <jaydenduong1202@gmail.com>
2026-07-04 08:12:28 +0800

    Find words

--- main.py
+++ main.py
@@ -49,18 +49,20 @@ def find_word(words, letters):
 
     count = 0
     while True:
-        count = 0
-        if rand(100 + (3 * 3) - (len(word) * 3) + (num_valid_words * 8)):
-            while True:
+        if rand(100 + (3 * 3) - (len(word) * 3) + (num_valid_words * 10)):
+            for i in range (180):
                 new_pos = [random.randint(0,3), random.randint(0,3)]
-                if abs(new_pos[0] - int(pos[count][0])) <= 1 and abs(new_pos[1] - int(pos[count][1])) <= 1 and len(list(dict.fromkeys(pos.append(new_pos)))) == len(pos):
+                if abs(new_pos[0] - int(pos[count][0])) <= 1 and abs(new_pos[1] - int(pos[count][1])) <= 1 and new_pos not in pos:
                     pos.append(new_pos)
                     word = word + letters[new_pos[0]][new_pos[1]]
                     break
+            else:
+                return valid_words
             
-            if len(word) >= 4 and word in words:
+            if word in words:
                 num_valid_words += 1
-                valid_words.append(word)
+                if len(word) >= 4:
+                    valid_words.append(word)
                 
                 #if rand(90):
                 #    pass
@@ -89,15 +91,19 @@ def find_word(words, letters):
 def search(words, runtime, letters):
     #st.write(letters)
     start_time = t.time()
-    valid_words = []
+    valid_words = set()
+    timer = st.empty()
+
     while (t.time() - start_time) < runtime:
-        val = find_word(words, letters)
-        if val not in valid_words:
-            valid_words.append(val)
-        #st.write("SEARCHING")
-        #t.sleep(1)
-    
-    st.write(valid_words)
+        remaining = runtime - (t.time() - start_time)
+        timer.write(f"Time remaining: {round(remaining, 2)}s")
+
+        found = find_word(words, letters)
+        valid_words.update(found)
+
+    sorted_words = sorted(valid_words, key=len, reverse=True)
+
+    st.write(sorted(sorted_words))
     
 
 def main():
