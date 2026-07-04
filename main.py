44987a70f3b97ab3e2a6a54eaba4d530a2950724
JD862 <jaydenduong1202@gmail.com>
2026-07-04 08:16:53 +0800

    Merge Fix

--- main.py
+++ main.py
@@ -18,11 +18,7 @@ def remv():
 
     return filtered_words
 
-def rand(val):
-    if random.randint(1, 100) <= val:
-        return True
-    else:
-        return False
+
 
 
 def convert(pos, letters):
@@ -49,27 +45,12 @@ def find_word(words, letters):
 
     count = 0
     while True:
-        if rand(100 + (3 * 3) - (len(word) * 3) + (num_valid_words * 10)):
-            for i in range (180):
-                new_pos = [random.randint(0,3), random.randint(0,3)]
-                if abs(new_pos[0] - int(pos[count][0])) <= 1 and abs(new_pos[1] - int(pos[count][1])) <= 1 and new_pos not in pos:
-                    pos.append(new_pos)
-                    word = word + letters[new_pos[0]][new_pos[1]]
-                    break
-            else:
-                return valid_words
-            
-            if word in words:
-                num_valid_words += 1
-                if len(word) >= 4:
-                    valid_words.append(word)
-                
-                #if rand(90):
-                #    pass
-                #else:
-                #    break
+        
 
-            count += 1
+        neighbors = find_neighbors(pos, count)
+        #st.write(neighbors, count)
+        if len(neighbors) > 0:
+            pos.append(random.choice(neighbors))
         else:
             break
 
@@ -90,26 +71,28 @@ def find_word(words, letters):
 
 def search(words, runtime, letters):
     #st.write(letters)
+
     start_time = t.time()
-    valid_words = set()
-    timer = st.empty()
-
+    valid_words = []
+    time_left = st.empty()
     while (t.time() - start_time) < runtime:
-        remaining = runtime - (t.time() - start_time)
-        timer.write(f"Time remaining: {round(remaining, 2)}s")
+        time_left.write(f"Time left: {round(runtime - t.time() + start_time, 1)}")
+        val = find_word(words, letters)
+        if val not in valid_words:
+            valid_words.append(val)
+        #st.write("SEARCHING")
+        #t.sleep(1)
 
-        found = find_word(words, letters)
-        valid_words.update(found)
-
-    sorted_words = sorted(valid_words, key=len, reverse=True)
-
-    st.write(sorted(sorted_words))
+    if None in valid_words:
+        valid_words.remove(None)
+    
+    st.write(sorted(valid_words, key=len, reverse=True))
     
 
 def main():
     st.title("Word Hunt Bot")
 
-    words = set(remv())
+    words = remv()
 
     #st.write(words)
     NULL = 0
